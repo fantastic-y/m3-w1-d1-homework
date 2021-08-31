4c1b5065f7ef4c0e17184e6661c463583d600795
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-08-30 22:30:46 -0700

    complete task2
    task 1 still remains the problem of not showing home page

--- server_task1.js
+++ server_task1.js
@@ -14,11 +14,18 @@ var server = http.createServer ((req,res) => {
     fs.readFile(filename, function(err, data) {
         if (err) {
             res.writeHead (404, {'Content-Type': 'text/html'});
-            return res.end ("Invalid Request!");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
+            res.end ("Invalid Request!");
+        } else if (q.pathname == '/contact.html') {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<html><body><h1>Contact Page.</h1></body></html>');
+        } else if (q.pathname == '/about.html') {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<html><body><h1>About Page.</h1></body></html>');
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('<html><body><h1>Home Page.</h1></body></html>');
+            fs.createReadStream(filename).pipe(res);
+        };
     });
 });
 
