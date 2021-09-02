44a4894705f264c2100dddefeb51557a13c083d8
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-09-01 18:02:37 -0700

    Update task1

--- server_task1.js
+++ server_task1.js
@@ -7,26 +7,23 @@ var hostname = 'localhost';
 var port = 5000;
 
 var server = http.createServer ((req,res) => {
-    
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead (404, {'Content-Type': 'text/html'});
-            res.end ("Invalid Request!");
-        } else if (q.pathname == '/contact.html') {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end('<html><body><h1>Contact Page.</h1></body></html>');
-        } else if (q.pathname == '/about.html') {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end('<html><body><h1>About Page.</h1></body></html>');
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.end('<html><body><h1>Home Page.</h1></body></html>');
-            fs.createReadStream(filename).pipe(res);
-        };
-    });
+    if (req.url === '/') {
+        res.setHeader("content-Type", "text/html");
+        res.writeHead(200);
+        res.end('<html><body><h1>Home Page.</h1></body></html>');
+    } else if (req.url === '/about') {
+        res.setHeader("content-Type", "text/html");
+        res.writeHead(200);
+        res.end('<html><body><h1>About Page.</h1></body></html>');
+    } else if (req.url === '/contact') {
+        res.setHeader("content-Type", "text/html");
+        res.writeHead(200);
+        res.end('<html><body><h1>Contact Page.</h1></body></html>');
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('Invalid Request!');
+    };
 });
 
 server.listen(port, hostname, () => {
